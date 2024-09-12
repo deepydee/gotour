59af957e89bd24798a4c9e426026a4b63fcb92c4
Vitalii Semenov <v.semenov@astrio.net>
2024-09-12 18:08:10 +0500

    basic types

---
+++ basics/basic_types/basic_types.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+	"math/cmplx"
+)
+
+func main() {
+	var (
+		toBe   bool       = false
+		maxInt uint64     = 1<<64 - 1
+		z      complex128 = cmplx.Sqrt(-5 + 12i)
+	)
+
+	fmt.Printf("Type: %T Value: %v\n", toBe, toBe)
+	fmt.Printf("Type: %T Value: %v\n", maxInt, maxInt)
+	fmt.Printf("Type: %T Value: %v\n", z, z)
+}
